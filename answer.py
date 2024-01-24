5a9b971529feb4270383d8026cd17ac774ea7380
Jayant Bangia <127044321+jayantb11@users.noreply.github.com>
2024-01-24 17:06:32 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -2,7 +2,7 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
-
+    print("Hello World")
     """
 
     # Write a comment; comments are very useful to other programmers.
@@ -12,6 +12,6 @@ def hello_world():
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    output =a+b
 
     return output
